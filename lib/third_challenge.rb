62085d661ceaaacf55d9f4b17c0fe5b9b94321a0
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-21 17:44:30 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
         :age => "15"
         :status => "alive"
         },
-      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
@@ -36,7 +36,7 @@ def third_challenge
         :age => "15"
         :status => "alive"
         },
-      :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"},{:name => "Nurse", :age => "44", :attitude => "worried"}]
+      :heroine_friends => []
    }
   }
 
